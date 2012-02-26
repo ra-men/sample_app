8135b8abde701ea3bfc800c797d80e0b50184912
Rhojel Echano <rhojel.echano@me.com>
2012-02-26 11:09:46 +0800

    Finish user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: valid_email_regex },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Henri Aragon"
+    email    "henny_p@fnf.com"
+    password "binjimon"
+  end
+end
